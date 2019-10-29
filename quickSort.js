48eb998c7b0bd37b91bd262f18a5677442b5cb5a
PirloLiu <pirlo@parkable.com>
2019-10-29 22:42:13 +1300

    add my quick sort function

--- quickSort.js
+++ quickSort.js
@@ -5,3 +5,27 @@
  * ▬▬ι═══════ﺤ            -═══════ι▬▬
  *
  */
+
+function quickSort(arr) {
+    if(arr.length <=1) return arr;
+
+    const middle = Math.floor(arr.length/2);
+    const pivot = arr.splice(middle,1)[0];
+
+    let less = [];
+    let greater = [];
+
+    for (let i = 0; i<arr.length; i++) {
+        if(arr[i] < pivot)
+            less.push(arr[i]);
+        else
+            greater.push(arr[i]);
+    }
+
+    return quickSort(less).concat(pivot, quickSort(greater));
+
+}
+
+const arr = [6,4,5,2,7];
+
+console.log(quickSort(arr));
