8df4be58a155161549855c15a4f0d95a0e791654
PirloLiu <pirlo@parkable.com>
2020-02-05 16:06:23 +0800

    Lazyman

--- lazyLoad.js
+++ lazyLoad.js
@@ -5,3 +5,66 @@
  * ▬▬ι═══════ﺤ            -═══════ι▬▬
  *
  */
+
+function LazyMan(name) {
+    return new _LazyMan(name);
+}
+
+class _LazyMan {
+    constructor(name) {
+        this.tasks = [];
+
+        const task = () => {
+            console.log(`Hi, This is ${name} !`);
+            this.next();
+        }
+
+        this.tasks.push(task);
+        setTimeout(()=> {
+            this.next();
+        }, 0)
+    }
+
+    next() {
+        const task = this.tasks.shift();
+        task && task();
+    }
+
+    eat(name) {
+        const task = () => {
+            console.log(`Eat ${name} ~~~`);
+            this.next();
+        }
+
+        this.tasks.push(task);
+        return this;
+    }
+
+    sleep(time) {
+        this._sleepWrapper(time, false);
+        return this;
+    }
+
+    sleepFirst(time) {
+        this._sleepWrapper(time, true);
+        return this;
+    }
+
+    _sleepWrapper(time, first) {
+        const task = () => {
+            setTimeout(() => {
+                console.log(`Wake up after ${time}`);
+                this.next();
+            }, time * 1000)
+        }
+
+        if(first) {
+            this.tasks.unshift(task)
+        } else {
+            this.tasks.push(task)
+        }
+    }
+}
+
+LazyMan("Hank").sleepFirst(2).eat("dinner");
+LazyMan("Hank").sleep(2).eat("dinner");
